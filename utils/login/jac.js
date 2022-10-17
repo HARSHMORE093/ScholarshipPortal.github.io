a9f2d65ab99177766e70331c32be481bbb0627e0
HARSHMORE093 <moreharsh093@gmail.com>
2022-10-17 15:21:47 +0530

    login1

---
+++ utils/login/jac.js
@@ -0,0 +1,21 @@
+// var login=document.getElementById('ak');
+// var p=new Element();
+// p=login;
+// login.parentNode.removeChild(login);
+
+function f(){
+var x=document.getElementById("for1");
+ x.style.opacity=0;
+ document.getElementById("ak").style.opacity=100;
+ document.getElementById("ak").style.zIndex=1;
+ x.style.zIndex=-1;
+
+}
+function tosinup(){
+    var x=document.getElementById("ak");
+     x.style.opacity=0;
+     document.getElementById("for1").style.opacity=100;
+     document.getElementById("for1").style.zIndex=1;
+     x.style.zIndex=-1;
+    
+    }
